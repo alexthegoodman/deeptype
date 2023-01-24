0f094f8e35432000a2d875f5b224315614b85fb0
Alex Goodman <alexthegoodman@gmail.com>
2023-01-24 16:04:37 -0500

    add basic og image

--- packages/commontext-web/components/Meta/Meta.tsx
+++ packages/commontext-web/components/Meta/Meta.tsx
@@ -21,11 +21,23 @@ const Meta: React.FC<MetaProps> = ({
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
   `;
 
+  const ogImage = "/aboutDeeptype.jpg";
+
   return (
     <>
       <title>{metaTitle}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content={description} />
+      <meta property="og:type" content="website" />
+      {/* <meta property="og:url" content="https://deeptype.app/"/> */}
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={ogImage} />
+      <meta property="twitter:card" content="summary_large_image" />
+      {/* <meta property="twitter:url" content="https://deeptype.app/"/> */}
+      <meta property="twitter:title" content={metaTitle} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={ogImage} />
       <link rel="icon" href="/favicon.ico" />
       <script src="https://unpkg.com/phosphor-icons" async></script>
       <Script
