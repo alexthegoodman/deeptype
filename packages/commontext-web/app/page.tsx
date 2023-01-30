9cad5d176d4f23e8558a9c4ca8b60a678576c6cf
Alex Goodman <alexthegoodman@gmail.com>
2023-01-30 14:06:22 -0500

    new demo video

--- packages/commontext-web/app/page.tsx
+++ packages/commontext-web/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
               <iframe
                 // width="560"
                 // height="315"
-                src="https://www.youtube.com/embed/E0x5n6m2a40?controls=0"
+                src="https://www.youtube.com/embed/7j7cjM1O1TQ?controls=0"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
