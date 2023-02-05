7226cb0786da087a1c5b1fd67c7c4fb19d0da5f2
Alex Goodman <alexthegoodman@gmail.com>
2023-02-04 22:06:38 -0500

    tweak

--- packages/commontext-web/components/Sidebars/Sidebars.tsx
+++ packages/commontext-web/components/Sidebars/Sidebars.tsx
@@ -6,8 +6,36 @@ import styles from "./Sidebars.module.scss";
 import { SidebarsProps } from "./Sidebars.d";
 import Information from "../Information/Information";
 import Suggestions from "../Suggestions/Suggestions";
+import useSWR from "swr";
+import graphClient from "../../helpers/GQLClient";
+import { getCurrentUserQuery } from "../../graphql/user";
+import { useCookies } from "react-cookie";
+
+const getUserData = async (token: string) => {
+  graphClient.setupClient(token);
+
+  const { getCurrentUser } = await graphClient.client?.request(
+    getCurrentUserQuery
+  );
+
+  return getCurrentUser;
+};
 
 const Sidebars: React.FC<SidebarsProps> = () => {
+  const [cookies, setCookie] = useCookies(["coUserToken"]);
+  const token = cookies.coUserToken;
+
+  const { data, error, isLoading, mutate } = useSWR(
+    "settings",
+    () => getUserData(token),
+    {
+      revalidateOnMount: true,
+    }
+  );
+
+  if (isLoading) return <></>;
+  if (error) return <>{error}</>;
+
   return (
     <Tabs className={styles.sidebars} selectedTabClassName={styles.selectedTab}>
       <TabList className={styles.sidebarsTabList}>
@@ -23,7 +51,11 @@ const Sidebars: React.FC<SidebarsProps> = () => {
         <Information />
       </TabPanel>
       <TabPanel>
-        <Suggestions />
+        {data.subscription === "PRO" ? (
+          <Suggestions />
+        ) : (
+          <p>Please upgrade to a Pro subscription use AI Text Suggestions</p>
+        )}
       </TabPanel>
     </Tabs>
   );
